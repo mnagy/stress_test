de02e75586b4b57a50c31e309cb22690769317fc
Martin Nagy <nagy.martin@gmail.com>
2016-03-16 17:11:56 +0100

    Update scripts

--- bytes_to_megs.py
+++ bytes_to_megs.py
@@ -1,11 +1,78 @@
 #!/usr/bin/python3
 
-import csv
+import matplotlib.pyplot as plt
+# import csv
 import sys
+import fileinput
+
 
 def to_mb(i):
     return str(round(int(i)/1024/1024, 3))
 
-for row in csv.reader(sys.stdin):
-    row = [x if int(x) < 1024 else to_mb(x) for x in row]
-    print(",".join(row))
+cols = []
+title = 'No title'
+fig_name = sys.argv[1] if len(sys.argv) > 1 else 'fig.png'
+label = 'Initial'
+x = []
+y = []
+data = {}
+# for row in csv.reader(sys.stdin):
+for line in fileinput.input():
+    data.setdefault(fileinput.filename(), {
+        'label': 'No label',
+        'x': [],
+        'y': []
+    })
+    d = data[fileinput.filename()]
+    if fileinput.isfirstline():
+        d['label'] = line.strip("# ")
+        params = map(lambda x: x.strip(), d['label'].split(','))
+        for p in params:
+            k, v = p.split('=')
+            d[k] = v
+    else:
+        row = line.split(',')
+        d['x'].append(int(row[0]))
+        d['y'].append(int(row[-1])/1024/1024)
+        # row = [x if int(x) < 1024 else to_mb(x) for x in row]
+        # cols.append(row)
+        # print(",".join(row))
+
+workers = {
+    '1': 'r',
+    '2': 'g',
+    '3': 'b',
+    '4': 'c',
+    '5': 'm',
+    '6': 'b',
+    '7': 'r',
+    '8': 'g'
+}
+sleep = {
+    '0': 'v',
+    '0.1': '',
+    '0.5': 'o',
+    '1': '^'
+}
+exp = {
+    '1': ':',
+    '10': '--',
+    '16': '-'
+}
+
+for d in data.values():
+    if d['exp'] == '16':
+        continue
+    f = "{0}{1}{2}".format(workers[d['workers']], sleep[d['sleep']],
+                           exp[d['exp']])
+    plt.plot(d['x'], d['y'], f, label=d['label'])
+
+for i in workers.keys():
+    x = range(2, 33, 2)
+    y = [50 + 15*int(i) + 0.1*a*int(i) for a in x]
+    plt.plot(x, y, '{0}-'.format(workers[i]))
+
+plt.title(title)
+# plt.legend()
+plt.show()
+# plt.savefig("/tmp/graph.png")
--- stress_test.sh
+++ stress_test.sh
@@ -4,7 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export stats_file=`mktemp /tmp/stress_test_stats.XXXX`
 export httperf_log_file=`mktemp /tmp/stress_test_httperf_log.XXXX`
 export container_args="-p 8080:8080"
 export label="ruby-stress-test"
@@ -54,39 +53,52 @@ function write_stats() {
   echo "${stats:-}" >> $stats_file
 }
 
-# Test function. Run httperf 3 times to make sure memory is maxed-out.
+# Test function. Run httperf 2 times to make sure memory is maxed-out.
 function http_test() {
   conns=$1; shift
   rate=$1; shift
   log_info ".. Testing with $conns connections, rate $rate"
-  for i in `seq 3`; do
+  for i in `seq 2`; do
     httperf $perf_args --num-conns $conns --rate $rate >> $httperf_log_file 2>&1
   done
   collect_stats
 }
 
 # Start
-log_info "Logging statistics into $stats_file"
+#log_info "Logging statistics into $stats_file"
 log_info "Logging httperf into $httperf_log_file"
 
-for i in `seq 32`; do
-  export threads=$i
-  export container_envs="-e PUMA_MAX_THREADS=$i"
 
-  create_container
+for exp in 1 10 16; do
+  for wrk in `seq 8`; do
+    #for slp in 0 0.5 1; do
+    for slp in 0.1; do
+      export stats_file="/tmp/run_04/run_${wrk}_${slp}_${exp}.csv"
+      echo "# sleep=$slp, workers=$wrk, exp=$exp" >> $stats_file
+      for i in `seq 2 2 32`; do
+        export threads=$i
+        export container_envs="-e PUMA_MAX_THREADS=$i -e TEST_SLEEP=$slp -e TEST_EXP=$exp -e TEST_WORKERS=$wrk"
 
-  # Run tests:
-  reset_stats
-  http_test 10000 1000
-  http_test 10000 2500
-  http_test 10000 5000
-  write_stats
+        create_container
 
-  log_info "Cleaning up $container"
-  remote docker stop $container >/dev/null
-  remote docker rm $container >/dev/null
+        # Run tests:
+        reset_stats
+        http_test 1000 100
+        http_test 1000 250
+        http_test 1000 500
+        write_stats
+
+        log_info "Cleaning up $container"
+        remote docker stop $container >/dev/null
+        remote docker rm $container >/dev/null
+      done
+      #cat $stats_file | /home/mnagy/work/stress_test/bytes_to_megs.py | tb
+    done
+  done
 done
 
+/home/mnagy/work/stress_test/bytes_to_megs.py /tmp/run_04/*.csv
+tbfoto /tmp/graph.png
 log_info "Statistics:"
-cat $stats_file
+#cat $stats_file
 log_info "End of $stats_file"
